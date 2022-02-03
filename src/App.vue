52eeee1d6e0c2d6a5bdfaeb7d67bbae943f28125
Zanco1993 <zanconatofabio@gmail.com>
2022-02-03 11:54:04 +0100

    header complete

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
-   
+   <header-app/>
+
   </div>
 </template>
 
 <script>
+import HeaderApp from './components/HeaderApp.vue'
+// import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
+    HeaderApp
   }
 }
 </script>
 
 <style lang="scss">
-
+@import './style/main.scss';
 </style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h1>BOOLFLIX</h1>
+
+    <div class="content-filter">
+      <input type="text" v-model="input" />
+      <button>Cerca</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: "",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./style/header.scss";
+</style>
